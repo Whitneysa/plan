04e91d4b1b832cb234ac3b79b814e1da8c1505a5
yhx <396940387@qq.com>
2023-05-29 15:46:50 +0800

    白名单缓存

--- users-center/src/main/java/com/zip/pojo/dto/LoginDTO.java
+++ users-center/src/main/java/com/zip/pojo/dto/LoginDTO.java
@@ -1,5 +1,7 @@
 package com.zip.pojo.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -11,20 +13,25 @@ import javax.validation.constraints.NotEmpty;
 
 @Data
 @Valid
+@ApiModel
 public class LoginDTO {
     @NotBlank
     @Length(min = 3,max = 20)
+    @ApiModelProperty("用户名")
     private String username;
 
     @NotBlank
     @Length(min = 6,max = 20)
+    @ApiModelProperty("密码")
     private String password;
 
     @NotBlank
+    @ApiModelProperty("验证码")
     private String code;
 
     @NotEmpty
     @Min(0)
     @Max(1)
+    @ApiModelProperty("是否记住密码:0不记住 1记住")
     private Integer remember;
 }
